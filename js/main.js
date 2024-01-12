b677fa69603b726872782618b4374562587dd53e
Andrey <kochergin_93@inbox.ru>
2024-01-12 20:45:30 +0300

    3

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import '../../node_modules/focus-visible/dist/focus-visible';
+import '../scss/main.scss';
+import '../index.html';
+import './open-menu';
+import './open-modal';
+import './open-more';
+import './side-menu';
+import './swiper';
